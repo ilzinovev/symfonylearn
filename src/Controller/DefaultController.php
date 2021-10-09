028fd38e90691483dbac68d234f3b5794c761b31
Your Name <ilzinovev@mail.ru>
2021-10-09 10:49:16 +0300

    Created base html.twig and hp

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="HomePage")
+     */
+    public function index(): Response
+    {
+        return $this->render('main/default/index.html.twig', [
+
+        ]);
+    }
+}
